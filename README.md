d2127692464de532bb105cd445a0285677344444
undefinedSynthesis <undefinedsynthesis@gmail.com>
2022-10-22 22:49:29 +0900

    セット (set) を学習

--- README.md
+++ README.md
@@ -2,9 +2,6 @@
 ### https://utokyo-ipp.github.io/
 ### lessons
 + 5-3. NumPyライブラリ
-+ 6-2. 高階関数
-+ 6-3. クラス
 + 7-1. pandasライブラリ
 + 7-2. scikit-learnライブラリ
-+ セット（set）
 + CSVファイルの入出力
--- src/set.py
+++ src/set.py
@@ -1,15 +1,25 @@
-def main():
-    s1 = {1, 2, 3}
-    s2 = {3, 4, 5}
-    print((s1 & s2))  # and
-    print(s1 | s2)  # or
-    print(s1 ^ s2)  # xor
-    print(s1 - s2)  # diff
-    print(s1.union(s2))  # union
-    print(s1.intersection(s2))  # and(Intersection)
-    print(s1.difference(s2))  # diff
-    intersection = s1.intersection(s2)
-    print((s1.union(s2)) - intersection)
+def check_characters(str1):
+    chars = set(str1)
+    return len(chars)
+
+def check_dicsize(dic1):
+    set_from_dic = set(dic1)
+    return len(set_from_dic)
+
+def count_words(str_engsentences):
+    symbols = ['.', ',', ':', ';', '!', '?']
+    str1 = str_engsentences
+    for symbol in symbols:
+        str1 = str1.replace(symbol, '')
+    return len(set(str1.split(' ')))
+
+def operation_sample():
+    set1 = {1, 2, 3, 4}
+    set2 = {3, 4, 5, 6}
+    print(set1.intersection(set2)) # 論理積
+    print(set1.union(set2)) # 論理和
+    print(set1.difference(set2)) # 差集合
+    print(set1.symmetric_difference(set2)) # 対称差
 
 if __name__ == "__main__":
-    main()
+    operation_sample()
