beefc6c142adc7ade1c3f5c695efe85c92b3bf65
undefinedSynthesis <undefinedsynthesis@gmail.com>
2022-10-22 22:49:51 +0900

    6-3. クラスを学習

--- src/class.py
+++ src/class.py
@@ -1,40 +1,46 @@
 import sys
-from decimal import Decimal
-class Book:
-    def __init__(self, name):
-        self.__name = name
-    def getName(self):
-        return self.__name
-    def setName(self, name):
-        self.__name = name
 
-class Money:
-    def __init__(self, amount):
-        self.__amount = Decimal(amount)
-    def getAmount(self):
-        return self.__amount
-    def add(self, other):
-        str1 = '{0:f}'.format(self.__amount + other.getAmount())
-        return Money(str1)
+class HelloForEver:
+    def readline(self):
+        return 'Hello. \n'
 
-    def subtract(self, other):
-        str1 = '{0:f}'.format(self.__amount - other.getAmount())
-        return Money(str1)
+class HelloFine:
+    def __init__(self, n):
+        self.n = n
+    def readline(self):
+        if self.n == 0:
+            return ''
+        self.n = self.n - 1
+        return 'Hello. \n'
 
-if __name__ == '__main__':
-    book = Book("戦争は女の顔をしていない")
-    print(book.getName())
-    print(book._Book__name)
+class HelloFile(HelloForEver):
+    def __init__(self, n):
+        self.n = n
+    def readline(self):
+        if self.n == 0:
+            return ''
+        self.n = self.n - 1
+        return super().readline()
 
-    money = Decimal("0.1")
-    other = Decimal("0.7")
-    added = money + other
-    print(type(added))
+class HelloFileIterator(HelloFile):
+    def __iter__(self):
+        return self
+    def __next__(self):
+        line = self.readline()
+        if line == '':
+            raise StopIteration
+        return line
 
-    money = Money("0.1")
-    other = Money("0.2")
-    added = money.add(other)
-    print(added.getAmount())
+class StringFileIterator(HelloFileIterator):
+    def __init__(self, s, n):
+        self.s = s
+        self.n = n
+    def readline(self):
+        if self.n == 0:
+            return ''
+        self.n = self.n - 1
+        return self.s
 
-    subtract = money.subtract(other)
-    print(subtract.getAmount())
+if __name__ == "__main__":
+    f = StringFileIterator('abc', 3)
+    print(list(f))
