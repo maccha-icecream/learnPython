c9e08804d823c1f9bfafedaa54aed9ca7ed2df6e
undefinedSynthesis <undefinedsynthesis@gmail.com>
2022-10-29 13:29:18 +0900

    プロパティ、クラスメソッド、スタティックメソッド、ダックタイピングを学習

--- src/class.py
+++ src/class.py
@@ -22,6 +22,7 @@ class HelloFile(HelloForEver):
         self.n = self.n - 1
         return super().readline()
 
+
 class HelloFileIterator(HelloFile):
     def __iter__(self):
         return self
@@ -30,6 +31,7 @@ class HelloFileIterator(HelloFile):
         if line == '':
             raise StopIteration
         return line
+
 
 class StringFileIterator(HelloFileIterator):
     def __init__(self, s, n):
@@ -41,6 +43,104 @@ class StringFileIterator(HelloFileIterator):
         self.n = self.n - 1
         return self.s
 
+class Person(object):
+    # コンストラクタ
+    def __init__(self, name):
+        self.name = name
+        print(self.name)
+
+    def say_something(self):
+        print(f'I\'m {self.name}.')
+        self.run(3)
+
+    def run(self, num):
+        print('run' * num)
+
+    # デストラクタ
+    def __del__(self):
+        print('good bye')
+
+
+class Car:
+    def __init__(self, model = None):
+        self.model = model
+    def run(self):
+        print('Run')
+
+
+class ToyotaCar(Car):
+    def __init__(self, model = "Model S", enable_auto_run = False):
+        super().__init__()
+        self.__enable_auto_run = enable_auto_run
+
+    @property
+    def enable_auto_run(self):
+        return self.__enable_auto_run
+
+    @enable_auto_run.setter
+    def enable_auto_run(self, is_enable):
+        self.__enable_auto_run = is_enable
+
+    def run(self):
+        print('fast')
+
+class TeslaCar(Car):
+    def auto_run(self):
+        print('Auto run')
+
+    def run(self):
+        print('Super fast')
+
+
+import math
+
+
+class Point:
+
+    count = 0
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.count += 1
+        print(f"you made {Point.count} instance(s).")
+
+
+    def __str__(self):
+        return f"(x, y) = ({self.x}, {self.y})"
+
+    @classmethod
+    def create_origin(cls):
+        return cls(0, 0)
+
+    @staticmethod
+    def calc_dist(point1, point2):
+        x = point1.x - point2.x
+        y = point1.y - point2.y
+        return math.sqrt(x ** 2 + y ** 2)
+
+class Word:
+    def __init__(self, text):
+        self.text = text
+
+    def __str__(self):
+        return f"This is {self.text}."
+
+    def __len__(self):
+        return len(self.text)
+
+    def __add__(self, other):
+        return self.text.lower() + other.text.lower()
+
+    def __eq__(self, other):
+        return self.text.lower() == other.text.lower()
+
+
 if __name__ == "__main__":
-    f = StringFileIterator('abc', 3)
-    print(list(f))
+    word = Word('test')
+    word2 = Word('test')
+
+    print(word)
+    print(len(word))
+    print(word + word2)
+    print(word == word2)
