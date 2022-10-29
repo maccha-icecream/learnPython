c7ca73934095e094a11ced00727522d45cdb792f
undefinedSynthesis <undefinedsynthesis@gmail.com>
2022-10-29 13:29:41 +0900

    ラムダ式の復習

--- src/lambda.py
+++ src/lambda.py
@@ -14,5 +14,10 @@ sample_func = lambda word: word.capitalize()
 def sample_func2(word):
     return word.lower()
 
-change_words(l, lambda word: word.capitalize())
-change_words(l, lambda word: word.lower())
+
+odd = [x for x in range(1, 11) if x % 2 == 1]
+# odd_between_5_and_10 = list(filter(lambda x:x > 4, odd))
+odd_between_5_and_10 = [x for x in odd if x > 4]
+print(odd_between_5_and_10)
+square_of_odds = [x ** 2 for x in odd]
+print(square_of_odds)
